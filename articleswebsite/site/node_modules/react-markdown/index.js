f5859b05eb844e2f80da88b39f98623c58f0a0ee
CamH04 <cameronahaynes04@gmail.com>
2025-01-16 13:50:09 +0000

    redirect to articles site

---
+++ articleswebsite/site/node_modules/react-markdown/index.js
@@ -0,0 +1,9 @@
+/**
+ * @typedef {import('./lib/index.js').AllowElement} AllowElement
+ * @typedef {import('./lib/index.js').Components} Components
+ * @typedef {import('./lib/index.js').ExtraProps} ExtraProps
+ * @typedef {import('./lib/index.js').Options} Options
+ * @typedef {import('./lib/index.js').UrlTransform} UrlTransform
+ */
+
+export {Markdown as default, defaultUrlTransform} from './lib/index.js'
